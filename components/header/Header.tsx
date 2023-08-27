62a355ad5f105436f7d3e9bfcfe6e82713d4a631
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2023-08-27 14:45:17 -0500

    :sparkles: create condition to show or hide phone case selector on nav

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,11 @@
+"use client";
 import {CartIcon, UserIcon} from "@/components/Icons";
 import {PhoneCaseTypeSelector} from "@/components/header/PhoneCaseTypeSelector";
+import {usePathname} from "next/navigation";
 
 export function Header() {
+    const pathname = usePathname()
+
     return (
         <div className="flex justify-center shadow-lg bg-white z-50">
             <div className="flex justify-start items-center container mx-10">
@@ -21,9 +25,11 @@ export function Header() {
                     </div>
 
                     <div className="flex justify-center items-center gap-1">
-                        <div className="pr-2">
-                            <PhoneCaseTypeSelector />
-                        </div>
+                        {pathname == "/" && (
+                            <div className="pr-2">
+                                <PhoneCaseTypeSelector />
+                            </div>
+                        )}
                         <div>
                             <UserIcon className="w-10 h-10 p-2 rounded-full cursor-pointer active:bg-gray-400/20 hover:bg-gray-400/10"/>
                         </div>
